9712ed64da1f9a87b618d4c831ca8231a73274fb
Urban <urbainishimwe@gmail.com>
2020-02-21 11:57:51 +0200

    dynamic array

--- hackerrank/main.go
+++ hackerrank/main.go
@@ -15,3 +15,20 @@ func ReverseArray(a []int) []int {
 	}
 	return a
 }
+
+//DynamicArray perform queries on dynamic array
+func dynamicArray(n int, queries [][]int) (answer []int) {
+	lastAnswer := int(0)
+	seqList := make([][]int, n)
+	for _, value := range queries {
+		N, x, y := value[0], value[1], value[2]
+		seq := (x ^ lastAnswer) % n
+		if N == 1 {
+			seqList[seq] = append(seqList[seq], y)
+		} else {
+			lastAnswer = seqList[seq][y%len(seqList[seq])]
+			answer = append(answer, lastAnswer)
+		}
+	}
+	return answer
+}
