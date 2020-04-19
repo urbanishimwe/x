3cb54cf5a3f578b0829bd3370b4b4599de84d5f9
Urban Ishimwe <urbainishimwe@gmail.com>
2020-04-19 12:33:38 +0200

    apple and orange

---
+++ hackerrank/apple_orange.go
@@ -0,0 +1,31 @@
+// question description at https://www.hackerrank.com/challenges/apple-and-orange/problem
+package main
+
+import (
+	"fmt"
+)
+
+// question description at https://www.hackerrank.com/challenges/apple-and-orange/problem
+func main() {
+	var S, T, A, B, M, N int
+	fmt.Scan(&S, &T, &A, &B, &M, &N)
+	Apple := make([]int, M)
+	Orange := make([]int, N)
+
+	appleOnHouse, orangeOnHouse := 0, 0
+	for i := range Apple {
+		fmt.Scan(&Apple[i])
+		if (Apple[i]+A) >= S && (Apple[i]+A) <= T {
+			appleOnHouse++
+		}
+	}
+	for i := range Orange {
+		fmt.Scan(&Orange[i])
+		if (Orange[i]+B) <= T && (Orange[i]+B) >= S {
+			orangeOnHouse++
+		}
+	}
+	fmt.Println(appleOnHouse)
+	fmt.Println(orangeOnHouse)
+
+}
